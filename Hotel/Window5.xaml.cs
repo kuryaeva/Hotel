5b609d426867a2a52fef423919d35f60b4bc0a73
Acer <Acer@DESKTOP-GG5PIOL>
2023-03-16 12:13:31 +0300

    Добавьте файлы проекта.

---
+++ Hotel/Window5.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Hotel
+{
+    /// <summary>
+    /// Логика взаимодействия для Window5.xaml
+    /// </summary>
+    public partial class Window5 : Window
+    {
+        public Window5()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Window2 window2 = new Window2();
+            window2.Show();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Registr_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Гость зарегистрирован", "Уведомление", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+    }
+}
